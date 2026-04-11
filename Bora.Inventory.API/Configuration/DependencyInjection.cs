2abe8bc5a1f36ca1d9b9e39ba79c4a3ed66440c2
LeoGrau <leonardo.grau@outlook.com.pe>
2026-04-11 12:42:30 -0500

    feat: adding

--- Bora.Inventory.API/Configuration/DependencyInjection.cs
+++ Bora.Inventory.API/Configuration/DependencyInjection.cs
@@ -6,28 +6,28 @@ namespace Bora.Inventory.API.Configuration;
 
 public static class DependencyInjection
 {
-    private static ApplicationOptions GenerateApplicationOption( IConfiguration configuration)
+    private static ApplicationOptions GenerateApplicationOption(IConfiguration configuration)
     {
         var autoMapperLicenseKey = configuration.GetSection("AutoMapper:LicenseKey").Value;
 
         if (string.IsNullOrEmpty(autoMapperLicenseKey)) throw new Exception("There is no license key for automapper.");
         
+        
         return ApplicationOptions.Build(autoMapperLicenseKey);
     }
-    
-    
+
+
     public static IServiceCollection AddApiServices(this IServiceCollection services,
         IConfiguration configuration)
     {
-        // Adding Infrastructure
-        services.AddInfrastructureLayer(configuration);
-        
         // Creating Application Options
         var applicationOptions = GenerateApplicationOption(configuration);
+        
+        // Adding Infrastructure Layer
+        services.AddInfrastructureLayer(configuration);
         
         // Adding Application Layer
         services.AddApplicationLayer(applicationOptions);
         return services;
     }
-
 }
